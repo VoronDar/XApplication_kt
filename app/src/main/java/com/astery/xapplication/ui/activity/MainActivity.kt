0ccaf3fb09aff901f25e8a67488550a09cceca16
Voronskaya Darya <56038397+VoronDar@users.noreply.github.com>
2021-11-09 15:53:02 +0300

    Initial commit

---
+++ app/src/main/java/com/astery/xapplication/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.astery.xapplication.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.astery.xapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
